f3c156bf8a436ee7842e61cfc8eb559c408fce8c
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-07-05 17:15:49 +0300

    Fixed call get info function after paste url

--- lib/core/widgets/custom_text_field.dart
+++ lib/core/widgets/custom_text_field.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:grabber/core/theme/app_colors.dart';
-import 'package:grabber/core/utils/app_services.dart';
-import 'package:grabber/features/home/presentation/view_model/home_screen_view_model.dart';
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final String? Function(String?)? validator;
+  final Widget? suffixIcon;
 
   const CustomTextField({
     super.key,
     required this.controller,
-    required this.validator,
     required this.hintText,
+    this.validator,
+    this.suffixIcon,
   });
 
   @override
@@ -31,27 +30,7 @@ class CustomTextField extends StatelessWidget {
       textInputAction: TextInputAction.done,
       validator: validator,
       autovalidateMode: AutovalidateMode.onUserInteraction,
-      decoration: InputDecoration(
-        hintText: hintText,
-        suffixIcon: _pasteButton(context),
-      ),
-    );
-  }
-
-  IconButton _pasteButton(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        AppServices.pasteFromClipboard(
-          context: context,
-          controller: controller,
-        );
-        context.read<HomeScreenViewModel>().getVideoInfo();
-      },
-      color: AppColors.darkTextColor,
-      splashColor: AppColors.darkHeadTextColor,
-      disabledColor: AppColors.darkHeadTextColor,
-      splashRadius: 20,
-      icon: const Icon(Icons.content_paste_rounded),
+      decoration: InputDecoration(hintText: hintText, suffixIcon: suffixIcon),
     );
   }
 }
--- lib/features/home/presentation/view/home_screen.dart
+++ lib/features/home/presentation/view/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:grabber/core/l10n/localization/app_localizations.dart';
+import 'package:grabber/core/theme/app_colors.dart';
+import 'package:grabber/core/utils/app_services.dart';
 import 'package:grabber/core/utils/app_validator.dart';
 import 'package:grabber/core/widgets/custom_text_field.dart';
 
@@ -21,6 +23,23 @@ class HomeScreen extends StatelessWidget {
               hintText: locale.url,
               controller: context.read<HomeScreenViewModel>().controller,
               validator: (value) => AppValidator.urlValidator(value),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  AppServices.pasteFromClipboard(
+                    context: context,
+                    controller: context.read<HomeScreenViewModel>().controller,
+                  );
+                  Future.delayed(Duration(seconds: 1), () {
+                    // ignore: use_build_context_synchronously
+                    context.read<HomeScreenViewModel>().getVideoInfo();
+                  });
+                },
+                color: AppColors.darkTextColor,
+                splashColor: AppColors.darkHeadTextColor,
+                disabledColor: AppColors.darkHeadTextColor,
+                splashRadius: 20,
+                icon: const Icon(Icons.content_paste_rounded),
+              ),
             ),
           ],
         ),
