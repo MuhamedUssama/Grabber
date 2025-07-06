1060de956fbb666db4a922e39fb177b959a3c62a
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-07-07 01:26:52 +0300

    Finished data layer in home screen feature

--- lib/features/home/data/data_source/home_data_source_impl.dart
+++ lib/features/home/data/data_source/home_data_source_impl.dart
@@ -5,7 +5,11 @@ import 'package:dio/dio.dart';
 import 'package:grabber/core/network/exceptions.dart';
 import 'package:grabber/core/network/retrofit_client.dart';
 import 'package:grabber/features/home/data/data_source/home_data_source.dart';
+import 'package:grabber/features/home/data/models/request/download_audio_request_model.dart';
+import 'package:grabber/features/home/data/models/request/download_video_request_model.dart';
 import 'package:grabber/features/home/data/models/request/get_video_info_request.dart';
+import 'package:grabber/features/home/data/models/response/download_audio_response_model.dart';
+import 'package:grabber/features/home/data/models/response/download_video_response_model.dart';
 import 'package:grabber/features/home/data/models/response/get_video_info_model.dart';
 import 'package:injectable/injectable.dart';
 
@@ -21,6 +25,73 @@ class HomeDataSourceImpl implements HomeDataSource {
   ) async {
     try {
       GetVideoInfoModel response = await _retrofitClient.getVideoInfo(url);
+      return Right(response);
+    } on DioException catch (exception) {
+      if (exception.type == DioExceptionType.connectionTimeout ||
+          exception.type == DioExceptionType.receiveTimeout ||
+          exception.type == DioExceptionType.sendTimeout) {
+        return const Left(NoInternetConnectionException());
+      } else if (exception.response != null) {
+        return Left(_handleStatusCode(exception.response!.statusCode));
+      } else {
+        log('exception: $exception');
+        return const Left(FetchDataException());
+      }
+    }
+  }
+
+  @override
+  Future<Either<ServerException, DownloadAudioResponseModel>> downloadAudio(
+    DownloadAudioRequestModel request,
+  ) async {
+    try {
+      DownloadAudioResponseModel response = await _retrofitClient.downloadAudio(
+        request,
+      );
+      return Right(response);
+    } on DioException catch (exception) {
+      if (exception.type == DioExceptionType.connectionTimeout ||
+          exception.type == DioExceptionType.receiveTimeout ||
+          exception.type == DioExceptionType.sendTimeout) {
+        return const Left(NoInternetConnectionException());
+      } else if (exception.response != null) {
+        return Left(_handleStatusCode(exception.response!.statusCode));
+      } else {
+        log('exception: $exception');
+        return const Left(FetchDataException());
+      }
+    }
+  }
+
+  @override
+  Future<Either<ServerException, DownloadVideoResponseModel>> downloadVideo(
+    DownloadVideoRequestModel request,
+  ) async {
+    try {
+      DownloadVideoResponseModel response = await _retrofitClient.downloadVideo(
+        request,
+      );
+      return Right(response);
+    } on DioException catch (exception) {
+      if (exception.type == DioExceptionType.connectionTimeout ||
+          exception.type == DioExceptionType.receiveTimeout ||
+          exception.type == DioExceptionType.sendTimeout) {
+        return const Left(NoInternetConnectionException());
+      } else if (exception.response != null) {
+        return Left(_handleStatusCode(exception.response!.statusCode));
+      } else {
+        log('exception: $exception');
+        return const Left(FetchDataException());
+      }
+    }
+  }
+
+  @override
+  Future<Either<ServerException, DownloadVideoResponseModel>>
+  downloadVideoWithoutAudio(DownloadVideoRequestModel request) async {
+    try {
+      DownloadVideoResponseModel response = await _retrofitClient
+          .downloadVideoWithoutAudio(request);
       return Right(response);
     } on DioException catch (exception) {
       if (exception.type == DioExceptionType.connectionTimeout ||
--- lib/features/home/data/repo_impl/home_repository_impl.dart
+++ lib/features/home/data/repo_impl/home_repository_impl.dart
@@ -1,7 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:grabber/core/network/exceptions.dart';
 import 'package:grabber/core/utils/connectivity_helper.dart';
+import 'package:grabber/features/home/data/models/request/download_audio_request_model.dart';
+import 'package:grabber/features/home/data/models/request/download_video_request_model.dart';
 import 'package:grabber/features/home/data/models/request/get_video_info_request.dart';
+import 'package:grabber/features/home/data/models/response/download_audio_response_model.dart';
+import 'package:grabber/features/home/data/models/response/download_video_response_model.dart';
 import 'package:grabber/features/home/data/models/response/get_video_info_model.dart';
 import 'package:grabber/features/home/domain/repository/home_repository.dart';
 import 'package:injectable/injectable.dart';
@@ -24,6 +28,53 @@ class HomeRepositoryImpl implements HomeRepository {
       return either.fold(
         (error) => Left(ServerException(error.message)),
         (videoInfo) => Right(videoInfo),
+      );
+    } else {
+      return Left(NoInternetConnectionException());
+    }
+  }
+
+  @override
+  Future<Either<ServerException, DownloadAudioResponseModel>> downloadAudio(
+    DownloadAudioRequestModel request,
+  ) async {
+    if (await ConnectivityHelper.checkInternetConnection()) {
+      final either = await _dataSource.downloadAudio(request);
+
+      return either.fold(
+        (error) => Left(ServerException(error.message)),
+        (filePath) => Right(filePath),
+      );
+    } else {
+      return Left(NoInternetConnectionException());
+    }
+  }
+
+  @override
+  Future<Either<ServerException, DownloadVideoResponseModel>> downloadVideo(
+    DownloadVideoRequestModel request,
+  ) async {
+    if (await ConnectivityHelper.checkInternetConnection()) {
+      final either = await _dataSource.downloadVideo(request);
+
+      return either.fold(
+        (error) => Left(ServerException(error.message)),
+        (filePath) => Right(filePath),
+      );
+    } else {
+      return Left(NoInternetConnectionException());
+    }
+  }
+
+  @override
+  Future<Either<ServerException, DownloadVideoResponseModel>>
+  downloadVideoWithoutAudio(DownloadVideoRequestModel request) async {
+    if (await ConnectivityHelper.checkInternetConnection()) {
+      final either = await _dataSource.downloadVideoWithoutAudio(request);
+
+      return either.fold(
+        (error) => Left(ServerException(error.message)),
+        (filePath) => Right(filePath),
       );
     } else {
       return Left(NoInternetConnectionException());
